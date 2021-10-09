200e49da0ed3d23c2c55345f3fed675e0edd0bfe
Gabriel Victor Raymundo <bieelvii@gmail.com>
2021-10-09 20:44:46 -0300

    refatado a view banco horas

--- view/listas/listar_banco_horas.php
+++ view/listas/listar_banco_horas.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/../begin.php'; ?>
 
-<div class="container mt-3">
+<section class="container mt-3">
 
     <div class="row">
         <div class="col-12">
@@ -9,17 +9,16 @@
             </a> 
         </div>
     </div>
+    
     <div class="row mt-3">        
-        <?php if(isset($anos)): ?>
-            <?php foreach($anos as $titulo): ?>
-                <div class="col-3 mx-auto" style="width: 200px;">
-                    <form action="/pesquisa-ano-selecionado" method="post">
-                        <input type="hidden" name="anoEscolhido" id="anoEscolhido" value="<?= $titulo['data_ano'] ?>">
-                        <button type="submit" class="btn btn-outline-secondary ps-5 pe-5 mx-auto" style="width: 200px;"> <?= $titulo['data_ano'] ?> </button>
-                    </form>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>        
+        <?php if(isset($anos)): ?> <?php foreach($anos as $titulo): ?>
+            <div class="col-3 mx-auto" style="width: 200px;">
+                <form action="/pesquisa-ano-selecionado" method="post">
+                    <input type="hidden" name="anoEscolhido" id="anoEscolhido" value="<?= $titulo['data_ano'] ?>">
+                    <button type="submit" class="btn btn-outline-secondary ps-5 pe-5 mx-auto" style="width: 200px;"> <?= $titulo['data_ano'] ?> </button>
+                </form>
+            </div>
+        <?php endforeach; ?> <?php endif; ?>        
     </div>
 
     <div class="row mt-3">
@@ -31,8 +30,8 @@
 
     <div class="row mt-3">
         <div class="col">
-            <?php if(isset($bancoHoras)): ?>
-                <?php foreach($bancoHoras as $horas): ?>
+            <?php if(isset($bancoHoras)): ?> <?php foreach($bancoHoras as $horas): ?>
+
                 <div class="accordion" id="accordionPanelsStayOpenExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="panelsStayOpen-heading<?= $horas[0]['id'] ?>">
@@ -41,41 +40,109 @@
                         </button>
                         </h2>
 
-                        <?php for($i = 0; $i < count($bancoHoras); $i++): ?>
-                        <?php foreach($bancoHoras[$i] as $horasDia): ?>
-                        <?php if($horas[0]['data_mes'] == $horasDia['data_mes']): ?>
+                        <div id="panelsStayOpen-collapse<?= $horas[0]['id'] ?>" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading<?= $horas[0]['id'] ?>">
+                             <div class="accordion-body">
 
-                            <div id="panelsStayOpen-collapse<?= $horas[0]['id'] ?>" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading<?= $horas[0]['id'] ?>">
-                                <div class="accordion-body">
-                                    <div class="row">
-                                        <div class="col">
-                                            <span> <b>Nome Empresa:</b> <?= $horasDia['nome_empresa'] ?> </span>
-                                            <span> <b>Horas Diárias:</b> <?= $horasDia['horas_trabalhadas_dia'] ?> </span><br>
-                                            <span> <b>Data:</b> <?= $horasDia['data_dia'].'/'.$horasDia['data_mes'].'/'.$horasDia['data_ano']?> </span>
-                                            <span> <b>Horário Entrada:</b> <?= $horasDia['horario_entrada'] ?> </span>
-                                            <span> <b>Horário Almoço:</b> <?= $horasDia['horario_entrada_almoco'] ?> </span>
-                                            <span> <b>Horário Retorno:</b> <?= $horasDia['horario_retorno_almoco'] ?> </span>
-                                            <span> <b>Horário Saída:</b> <?= $horasDia['horario_saida'] ?> </span>
-                                            <span> <b>Hora Extra (minutos):</b> <?= $horasDia['horas_totais_minutos'].' min' ?> </span><br>
-                                            <span> <b>Observações:</b> <?= $horasDia['observacao'] ?> </span><br>
-                                            <span> <b>Excessão:</b> <?= $horasDia['excessao'] ?> </span>
+                                <div class="row">
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <strong>Data</strong>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <strong>Horário Entrada</strong>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <strong>Horário Almoço</strong>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <strong>Horário Retorno</strong>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <strong>Horário Saída</strong>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <strong>Horas Extras (min)</strong>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <strong>Observações</strong>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <strong>Jornada de Trabalho</strong>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
 
-                        <?php endif; ?>
-                        <?php endforeach; ?>
-                        <?php endfor; ?>
+                                <?php for($i = 0; $i < count($bancoHoras); $i++): ?> <?php foreach($bancoHoras[$i] as $horasDia): ?> <?php if($horas[0]['data_mes'] == $horasDia['data_mes']): ?>
+
+                                <div class="row">
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <span><?= $horasDia['data_dia'].'/'.$horasDia['data_mes'].'/'.$horasDia['data_ano']?></span>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <span><?= $horasDia['horario_entrada'] ?></span>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <span><?= $horasDia['horario_entrada_almoco'] ?></span>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <span><?= $horasDia['horario_retorno_almoco'] ?></span>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <span><?= $horasDia['horario_saida'] ?></span>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <span><?= $horasDia['horas_totais_minutos'] ?></span>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <span><?= $horasDia['observacao'] ?></span>
+                                        </div>
+                                    </div>
+                                    <div class="col border bg-light">
+                                        <div class="text-center">
+                                            <span><?= $horasDia['horas_trabalhadas_dia'] ?></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                    
+                                <?php endif; ?> <?php endforeach; ?> <?php endfor; ?>
+
+                            </div>
+                        </div>                        
 
                     </div>
                 </div>
                 
-                <?php endforeach; ?>
-            <?php endif; ?>
+            <?php endforeach; ?> <?php endif; ?>
         </div>
     </div>
 
-</div>
+</section>
 
 <?php include __DIR__ . '/../end.php'; ?>
